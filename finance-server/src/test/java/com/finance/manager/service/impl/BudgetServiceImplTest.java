8fc650ade2c50a9a96423b5cd7ba5afbae278747
jlh321 <jlh123@foxmail.com>
2020-09-16 23:56:30 +0800

    add UnitTest

--- finance-server/src/test/java/com/finance/manager/service/impl/BudgetServiceImplTest.java
+++ finance-server/src/test/java/com/finance/manager/service/impl/BudgetServiceImplTest.java
@@ -1,42 +1,95 @@
 package com.finance.manager.service.impl;
 
+import com.finance.manager.controller.BudgetController;
+import com.finance.manager.entity.Budget;
+import com.finance.manager.entity.Category;
+import com.finance.manager.repository.BudgetRepository;
+import com.finance.manager.service.BudgetService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 class BudgetServiceImplTest {
+    private BudgetRepository budgetRepository;
+    private BudgetService test;
 
     @BeforeEach
     void setUp() {
+        budgetRepository = mock(BudgetRepository.class);
+        test = new BudgetServiceImpl(budgetRepository);
+
     }
 
-    @AfterEach
-    void tearDown() {
-    }
+
 
     @Test
     void getBudgetSumByMonth() {
+//        Double budgetDouble = 10.0;
+        List<Budget> list = new ArrayList<>();
+        list.add(new Budget(12D, new Category("asd", "sd"), 10, 202));
+        doReturn(list).when(budgetRepository).getBudgetByMonthIsAndYearIs(anyInt(), anyInt());
+        Double result = test.getBudgetSumByMonth(10, 2020);
+        assertNotNull(result);
     }
 
     @Test
     void getBudgetByMonth() {
+        List<Budget> list = new ArrayList<>();
+        list.add(new Budget(12D, new Category("asd", "sd"), 10, 202));
+        doReturn(list).when(budgetRepository).getBudgetByMonthIsAndYearIs(anyInt(), anyInt());
+        List<Budget> result = test.getBudgetByMonth(10, 2020);
+        assertNotNull(result);
+//
     }
-
+    //
     @Test
     void deleteBudget() {
+        Budget budget = new Budget(12D, new Category("asd", "sd"), 10, 202);
+        doNothing().when(budgetRepository).delete(any(Budget.class));
+        Budget result = test.deleteBudget(budget);
+        assertNotNull(result);
     }
 
     @Test
     void addBudget() {
-    }
-
-    @Test
-    void existsById() {
+        Budget budget = new Budget(12D, new Category("asd", "sd"), 10, 202);
+        doReturn(budget).when(budgetRepository).save(any(Budget.class));
+        Budget result = (Budget) test.addBudget(budget);
+        assertNotNull(result);
     }
 
     @Test
     void putBudget() {
+        Budget budget = new Budget(12D, new Category("asd", "sd"), 10, 202);
+        doReturn(budget).when(budgetRepository).save(any(Budget.class));
+        Budget result = (Budget) test.putBudget(budget);
+        assertNotNull(result);
+
     }
+
+    @Test
+    void existsById() {
+//        Budget budget = new Budget(12.D, new Category("1", "Rent"), 2019, 11);
+//        budget.setId("12");
+//
+//        doReturn(budget).when(budgetRepository).existsById(anyString());
+//        Boolean result = test.existsById("12");
+//        assertNotNull(result);
+
+    }
+
+
+
+
 }
--- finance-server/src/test/java/com/finance/manager/service/impl/CategoryServiceImplTest.java
+++ finance-server/src/test/java/com/finance/manager/service/impl/CategoryServiceImplTest.java
@@ -1,38 +1,68 @@
 package com.finance.manager.service.impl;
 
 import com.finance.manager.entity.Category;
+import com.finance.manager.repository.BudgetRepository;
+import com.finance.manager.repository.CategoryRepository;
 import com.finance.manager.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
-@SpringBootTest
 class CategoryServiceImplTest {
+    private CategoryRepository categoryRepository;
+    private CategoryService test;
 
-    @Autowired
-    private CategoryService categoryService;
+    @BeforeEach
+    void setUp() {
+        categoryRepository = mock(CategoryRepository.class);
+        test = new CategoryServiceImpl(categoryRepository);
+
+    }
+
 
     @Test
     void getAllCategories() {
-        List<Category> categoryList = categoryService.getAllCategories();
-        for(Category category: categoryList){
-            System.out.println(category);
-        }
+        List<Category> categoryList = new ArrayList<>();
+        doReturn(categoryList).when(categoryRepository).findAll();
+        List<Category> result = test.getAllCategories();
+        assertNotNull(result);
+
     }
 
     @Test
     void setCategory() {
+        Category category = new Category();
+        doReturn(category).when(categoryRepository).save(any(Category.class));
+        Category result = test.setCategory(category);
+        assertNotNull(result);
     }
 
     @Test
     void deleteCategory() {
+//        Category category = new Category("1111","2222");
+//        doNothing().when(categoryRepository).deleteById(anyString());
+//
+//        doReturn(category).when(categoryRepository).findById(anyString());
+//        Category result = test.deleteCategory(category.getId());
+//        assertNotNull(result);
+
     }
 
     @Test
     void existsById() {
+        Category category = new Category("1111","2222");
+        doReturn(Boolean.TRUE).when(categoryRepository).existsById(anyString());
+        boolean result = test.existsById("1111");
+        assertNotNull(result);
     }
 }
